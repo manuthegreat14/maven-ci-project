c38b8aa219a6c86009d881bb214089e53da32b37
manuthegreat14 <manuthegreat14@gmail.com>
2026-05-04 10:28:43 +0000

    Initial commit - Maven project with CI setup

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import junit.framework.TestCase;
+
+public class AppTest extends TestCase {
+
+    public void testAdd() {
+        assertEquals(5, App.add(2, 3));
+    }
+}
